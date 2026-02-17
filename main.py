97cf4eceeed8b0def303ec6b0ba512e3afb38715
Christopher Ponds <87038488+ShowMeYourLegos96@users.noreply.github.com>
2026-02-17 23:11:03 +0000

    Modifying the GetInput Method in the Menu class

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+
 from menu import Menu
+#if __name__ == '__main__':
+  #m = main()
 
-
+#call the class
 mainMenu = Menu()
 
+#call the methods in the class to add in the options
 mainMenu.addOption("Check available memory")
 mainMenu.addOption("View Network Connections")
 mainMenu.addOption("Display free ram and swap")
 mainMenu.addOption("Quit")
 
+#calling the getUserInput method to collect the user input
 choice = mainMenu.getInput()
--- menu.py
+++ menu.py
@@ -1,23 +1,28 @@
-import os
 import run_bash_cmd_function
 class Menu:
     def __init__(self):
         self.options = []
     
-    def addOption(self,option):
+    def addOption(self, option):
         self.options.append(option)
-
+        
     
     def getInput(self):
-        GI = input(f'{len(self.options)}')
-        while GI == 1 or 2 or 3 or 4:
-            GI = input(f'{len(self.options)}')
-            if GI != 1 or 2 or 3 or 4:
-             print("Please enter the correct value")
-            elif GI == 4:
+        
+        for GI in self.options:
+            #print(f'{self.options}')
+            GI = int(input(f'{self.options}')) #makes this string into integer
+            print("GI type is ", type(GI)) #type is now an integer
+
+            if GI == 1 or 2 or 3:#checks if the input is 1,2, or 3 
+             run_bash_cmd_function(GI)#will run the function 
+             continue #will repeat the prompt for the user to enter again
+
+            elif GI == 4: #if user press quit
                 break
-            else:
-                run_bash_cmd_function(GI)
+            else: #if the user picks an invalid input
+                print("Please enter the correct value")
+                continue
         #self.input = input(self.options)
         #self.input = int(input)
     
